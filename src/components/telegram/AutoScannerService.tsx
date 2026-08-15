a2270204645e899606bdf5e885db5d4ebfc818aa
Dyad <luffyhales1@gmail.com>
2026-08-16 00:02:40 +0500

    Fixed lucide-react Image imports by adding aliases (Image as ImageIcon) in TelegramBotSimulator.tsx, SignalCard.tsx, AutoScannerService.tsx, and GrokMarketBot.tsx. Also fixed Globe import in GrokMarketBot.tsx (Globe as GlobeIcon). Fixed unclosed span tag in Index.tsx and unclosed div tag in SignalCard.tsx. - wrote 5 file(s)

--- src/components/telegram/AutoScannerService.tsx
+++ src/components/telegram/AutoScannerService.tsx
@@ -329,7 +329,7 @@ export const AutoScannerService: React.FC = () => {
                   </Badge>
                 </div>
               </div>
-            ))}
+            )}
           </div>
         )}
       </div>
